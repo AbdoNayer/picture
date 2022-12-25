6110828c9b7300ac4c5cf0b592c2b290b414de18
AbdoNayer <abdelwahabnayer20@gmail.com>
2022-12-25 20:15:07 +0200

    done-page

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     });
 
     // Scroll For Section
-    $(document).on("click", ".header ul li a",function(e) {
+    $(document).on("click", "header ul li a",function(e) {
         $('html, body').animate({
             scrollTop: $('#' + $(this).data('value')).offset().top - 100
         }, 1000);
